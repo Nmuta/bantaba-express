65b0e21b093c8aa503dff6b8898ba88a99cd4b76
amespineira <amespineira@me.com>
2016-10-05 11:03:35 -0600

    still working on ionic auth

--- routes/auth.js
+++ routes/auth.js
@@ -74,7 +74,7 @@ router.get('/ionic', function(req, res) {
   var state = req.query.state;
   console.log(req);
   try {
-    var incomingToken = nJwt.verify(req.query.token, mySharedSecret);
+    var incomingToken = nJwt.verify(req.query.token, secretKey);
   } catch (ex) { // lots of stuff can go wrong while decoding the jwt
     console.error(ex.stack);
     return res.status(401).send('jwt error');
