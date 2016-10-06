5e50930d90c6834fd4345c9a3475b77bdaa26770
amespineira <amespineira@me.com>
2016-10-06 14:17:05 -0600

    fixing date formatting

--- queries/performers.js
+++ queries/performers.js
@@ -46,7 +46,7 @@ module.exports={
     })
   },
   createNotification:function(specs, performer_id){
-    return knex('performer_notifications').insert({performer_id:performer_id, text:specs.text, posted:specs.date})
+    return knex('performer_notifications').insert({performer_id:performer_id, text:specs.text, posted:new Date(specs.date)})
   },
   getFromUser:function(user_id){
     return knex('performers').where({user_id:user_id})
